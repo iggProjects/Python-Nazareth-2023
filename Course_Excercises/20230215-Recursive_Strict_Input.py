f3cce156410a70e1ac31c96b3c5ad9e293eed5de
iggProjects <100952541+iggProjects@users.noreply.github.com>
2023-02-15 15:32:35 +0100

    Create 20230215-Recursive_Strict_Input.py
    Example of checking input data until you do rigth

---
+++ Course_Excercises/20230215-Recursive_Strict_Input.py
@@ -0,0 +1,22 @@
+print('\n--------------INPUT WITH INTEGER CONDITION-----------------\n')
+
+def input_as_int():
+    try:
+        user_age = int(input('how old are you? '))
+        user_brothers = int(input('how many brothers do you have? '))
+        print()
+#       if all(x>=0 for x in [user_age,user_brothers]):
+        if 0 < user_age < 150:
+            print(f"Your age is: {user_age}, and you have {user_brothers} brothers")
+            if user_brothers == 0:
+               print('you are a single person\n')
+            elif 1 < user_brothers < 6:
+               print("You are lucky person")
+            else:
+               print('WOW, you have a LOT of problems')
+        else: raise ValueError
+    except ValueError:
+        print('Please, enter an integer')
+        input_as_int()
+
+input_as_int()
