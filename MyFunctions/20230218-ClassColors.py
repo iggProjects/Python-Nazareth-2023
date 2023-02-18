16cc8ddef3d2733d7131826a3563f7a6f8970312
InakiGainza <igg.git.h@gmail.com>
2023-02-18 23:28:38 +0100

    ClassColors added

---
+++ MyFunctions/20230218-ClassColors.py
@@ -0,0 +1,105 @@
+"""
+https://www.geeksforgeeks.org/print-colors-python-terminal/
+https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+https://www.javatpoint.com/how-to-print-colored-text-in-python
+https://gist.github.com/iansan5653/c4a0b9f5c30d74258c5f132084b78db9
+
+Class COLORS for printing in console
+
+Colors class:
+    reset all colors with colors.reset;
+    two sub classes fg for foreground and bg for background; 
+    use as colors.subclass.colorname.
+        i.e. colors.fg.red or colors.bg.greenalso, the generic bold, disable, underline, 
+        reverse, strike through, and invisible work with the main class i.e. colors.bold
+"""
+
+class colors:
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m' 
+
+    class fg:
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m' 
+    class bg:
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m' 
+
+"""
+prints table of formatted text format options
+"""
+def print_format_table():
+    for style in range(8):
+        for fg in range(30, 38):            
+            s1 = ''
+            for bg in range(40, 48):
+                format = ';'.join([str(style), str(fg), str(bg)])                
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+                print(f"{format} -> {s1}")                
+                #print(f"\t{s1}")
+            #print(s1)            
+        print('\n')
+
+def print_format_table1():
+    for style in range(1):
+        for fg in range(30, 31):            
+            s1 = ''
+            for bg in range(40, 48):
+                format = ';'.join([str(style), str(fg), str(bg)])                
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+                print(f"{format} -> {s1}")                
+                #print(f"\t{s1}")
+            #print(s1)            
+        print('\n')
+
+
+print(colors.bg.green, "SKk", colors.fg.red, "Amartya") 
+
+#print(colors.bg.blue, colors.fg.red, "SKk Amartya",)
+print(colors.bg.blue, "SKk", colors.fg.red, "Amartya")
+print(colors.reset)
+
+#print_format_table()
+print_format_table1()
+
+print('\x1b[0m Reset / Normal \x1b[0m')
+print('\x1b[21m Doubly underline or Bold off \x1b[0m')
+print('\x1b[31m Red foreground \x1b[0m')
+print('\t\x1b[47m White background \x1b[0m')
+print('\t\x1b[102m Bright Green background \x1b[0m')
+
+print(colors.reset)
+
+printString = ''
+bgch = ''
+for i in range(0, 16):
+    printString += f'\x1b[48;5;{i}m{bgch} abc \x1b[0m'
+print(printString)
+
+CSI = "\x1B["
+print(CSI+"31;44m" + "Colored Text" + CSI + "0m")
+
