2cd88a8feea2596d88c8a2478bb74ad1ee9c642c
InakiGainza <igg.git.h@gmail.com>
2023-02-18 01:08:43 +0100

    testing import from sub folder and parallel folder

--- Course_Excercises/zPruebaImportFunctions1.py
+++ Course_Excercises/zPruebaImportFunctions1.py
@@ -1,5 +1,6 @@
 # probando la sentencia "from file import function"
 #
+# https://sweetcode.io/python-file-importation-multi-level-directory-modules-packages/
 # https://www.geeksforgeeks.org/python-import-module-from-different-directory/
 # https://es.stackoverflow.com/questions/401804/python-importerror-attempted-relative-import-with-no-known-parent-package
 # https://problemsolvingwithpython.com/07-Functions-and-Modules/07.05-Calling-Functions-from-Other-Files/#:~:text=To%20use%20the%20functions%20written,of%20the%20filename%20during%20import.
@@ -10,15 +11,15 @@
 ## calling zFunctions from its folder
 ## Create file __init__.py empty in that folder - is for read dir
 
+"""
+https://codeolives.com/2020/01/10/python-reference-module-in-parent-directory/
 import sys
-# caution: path[0] is reserved for script path (or '' in REPL)
-#sys.path.insert(0, '\ML_Project\Python-igg\Modulo-2-Sgf\igg\MyFunctions')
-from zFunctions import *
-import Colors
-
+sys.path.append('..')
+from ..MyFunctions.zFunctions import * 
+from ..MyFunctions.Colors import *
 """
 
-
+"""
 def Purple(mens): print("\033[95m{}\033[00m\n" .format(mens)) 
 def Yellow(mens): print("\033[93m{}\033[00m\n" .format(mens)) 
 
--- zPruebaImportFunctions0.py
+++ zPruebaImportFunctions0.py
@@ -1,5 +1,6 @@
 # probando la sentencia "from file import function"
 #
+# https://sweetcode.io/python-file-importation-multi-level-directory-modules-packages/
 # https://www.geeksforgeeks.org/python-import-module-from-different-directory/
 # https://es.stackoverflow.com/questions/401804/python-importerror-attempted-relative-import-with-no-known-parent-package
 # https://problemsolvingwithpython.com/07-Functions-and-Modules/07.05-Calling-Functions-from-Other-Files/#:~:text=To%20use%20the%20functions%20written,of%20the%20filename%20during%20import.
@@ -10,14 +11,14 @@
 ## calling zFunctions from its folder
 ## Create file __init__.py empty in that folder - is for read dir
 
-import sys
+#import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-#sys.path.insert(0, '\ML_Project\Python-igg\Modulo-2-Sgf\igg\MyFunctions')
-from zFunctions import *
-import Colors
+#sys.path.insert(0, '../MyFunctions/')
+#sys.path.insert(0, 'C:\ML_Project\NazarethCourse2023\Python-Nazareth-2023\MyFunctions')
+from MyFunctions.zFunctions import mostrar,ver_objetos
+from MyFunctions.Colors import *
 
 """
-
 
 def Purple(mens): print("\033[95m{}\033[00m\n" .format(mens)) 
 def Yellow(mens): print("\033[93m{}\033[00m\n" .format(mens)) 
