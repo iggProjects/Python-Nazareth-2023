53235f8a3d7173397ae42bb67633bf2fd7404dba
InakiGainza <igg.git.h@gmail.com>
2023-02-20 00:14:32 +0100

    update colors scrìpts

--- MyFunctions/20230218-ClassColors.py
+++ MyFunctions/20230218-ClassColors.py
@@ -3,6 +3,9 @@ https://www.geeksforgeeks.org/print-colors-python-terminal/
 https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
 https://www.javatpoint.com/how-to-print-colored-text-in-python
 https://gist.github.com/iansan5653/c4a0b9f5c30d74258c5f132084b78db9
+https://gist.github.com/vratiu/9780109
+
+
 
 Class COLORS for printing in console
 
@@ -58,7 +61,7 @@ def print_format_table():
             s1 = ''
             for bg in range(40, 48):
                 format = ';'.join([str(style), str(fg), str(bg)])                
-                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, 'hola')
                 print(f"{format} -> {s1}")                
                 #print(f"\t{s1}")
             #print(s1)            
@@ -83,8 +86,8 @@ print(colors.bg.green, "SKk", colors.fg.red, "Amartya")
 print(colors.bg.blue, "SKk", colors.fg.red, "Amartya")
 print(colors.reset)
 
-#print_format_table()
-print_format_table1()
+print_format_table()
+# print_format_table1()
 
 print('\x1b[0m Reset / Normal \x1b[0m')
 print('\x1b[21m Doubly underline or Bold off \x1b[0m')
@@ -95,13 +98,14 @@ print('\t\x1b[102m Bright Green background \x1b[0m')
 print(colors.reset)
 
 printString = ''
-bgch = ''
+bgch = '--'
 for i in range(0, 16):
     printString += f'\x1b[48;5;{i}m{bgch} abc \x1b[0m'
 print(printString)
 
-CSI = "\x1B["
-print(CSI+"31;94m" + "Colored Text" + CSI + "0m")
+CSI = "\x1b["
+print("\n" + CSI + "31;94m" + "Colored Text" + CSI + "0m")
+print(f"\n{CSI}31;92mColored Text{CSI}0m")
 
 def colors_text256(color_):  
     num1 = str(color_)  
@@ -110,6 +114,37 @@ def colors_text256(color_):
         return(f"\033[48;5;{num1}m {num2} \033[0;0m\n")  
     else:  
         return(f"\033[48;5;{num1}m {num2} \033[0;0m")  
-  
+
+ 
 print("\nThe 256 colors scheme is:")  
-print('--'.join([colors_text256(x) for x in range(256)]))  
+print('--'.join([colors_text256(x) for x in range(256)]))
+
+
+# testing some values
+print(''.join([colors_text256(16)]))
+print(''.join([colors_text256(17)]))
+
+# print(colors_text256(41))
+
+
+def msg_colors_text256(msg,color_):  
+    num1 = str(color_)  
+    #num2 = str(color_).ljust(3, ' ')  
+    if color_ % 16 == 0:  
+        return(f"\033[48;5;{num1}m {msg} \033[0;0m\n")  
+    else:  
+        return(f"\033[48;5;{num1}m {msg} \033[0;0m")
+    
+"""
+ 16: bg black fg white
+ 17: bg blue  fg white
+124: bg red   fg white
+"""
+print(msg_colors_text256('hy',16))
+print(msg_colors_text256('hy',17))
+print(msg_colors_text256('hy',124))
+print(msg_colors_text256('hy',208))
+
+print("\033[1;43m HELLO \n")
+
+print(colors.reset)
--- MyFunctions/Colors.py
+++ MyFunctions/Colors.py
@@ -2,18 +2,19 @@
             😎  My colors for printing  😎
 """
 
-
-def Purple(mens): print("\033[95m{}\033[00m\n" .format(mens)) 
-def Yellow(mens): print("\033[93m{}\033[00m\n" .format(mens)) 
-
 # https://www.geeksforgeeks.org/print-colors-python-terminal/
 # colored text and background 
+# fore
+def prRed(msg):         print("\033[91m{}\033[00m".format(msg)) 
+def prGreen(msg):       print("\033[92m{}\033[00m".format(msg)) 
+def prYellow(msg):      print("\033[93m{}\033[00m".format(msg)) 
+def prLightPurple(msg): print("\033[94m{}\033[00m".format(msg)) 
+def prPurple(msg):      print("\033[95m{}\033[00m".format(msg)) 
+def prCyan(msg):        print("\033[96m{}\033[00m".format(msg)) 
+def prLightGray(msg):   print("\033[97m{}\033[00m".format(msg)) 
+def prBlack(msg):       print("\033[98m{}\033[00m".format(msg)) 
 
-def prRed(skk): print("\033[91m{}\033[00m" .format(skk)) 
-def prGreen(skk): print("\033[92m{}\033[00m" .format(skk)) 
-def prYellow(skk): print("\033[93m{}\033[00m" .format(skk)) 
-def prLightPurple(skk): print("\033[94m{}\033[00m" .format(skk)) 
-def prPurple(skk): print("\033[95m{}\033[00m" .format(skk)) 
-def prCyan(skk): print("\033[96m{}\033[00m" .format(skk)) 
-def prLightGray(skk): print("\033[97m{}\033[00m" .format(skk)) 
-def prBlack(skk): print("\033[98m{}\033[00m" .format(skk)) 
+prCyan('HOLA')
+print('HOLA')
+prRed('HOLA')
+prBlack('HOLA')
