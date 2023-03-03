a36a92045b9cf54cd770f87f37aee71a77180aae
InakiGainza <igg.git.h@gmail.com>
2023-03-03 21:34:30 +0100

    update prototype for teacher

--- Teacher_Folder/0-prototype_scrip_ForTeacher.py
+++ Teacher_Folder/0-prototype_scrip_ForTeacher.py
@@ -34,16 +34,24 @@ if __name__ == "__main__":
     while yesss:
         _msg = "Do you want to see attributes for a specific VAR ? (Y,N): "
         answer=Y_N_2(_msg)        
-        # add question for name of var.....
-        _what_var = str(input("What VAR?"))
-        _my_Obj_name = eval(_what_var)
         if answer in ['Y','N']: yesss = False
 
     if answer == 'Y':            
-        print(f"\n{FR_GREEN}---------- INFO FOR OBJECT '{_my_Obj_name}' ----------{NO_COLOR}\n")
-        pause()
-        # my objects functions  
-        mostrar(_my_Obj_name)       
+        # add question for name of var.....
+        _what_var = str(input("What VAR ? "))
+        try: 
+            _what_var
+            _my_Obj_name = eval(_what_var)
+            print(f"\n{FR_GREEN}---------- INFO FOR OBJECT '{_my_Obj_name}' ----------{NO_COLOR}\n")
+            pause()
+            # my objects functions  
+            mostrar(_my_Obj_name)       
+
+        except NameError:
+            print(f"\n\t{FR_RED}---- Var '{_what_var}' doesn't exits 🙄🙄  ----")
+            print(f"\n{FR_GREEN}--------------- That's all for today 👌 ---------------{NO_COLOR}\n")
+            #_my_Obj_name = None 
+
     else:
         print(f"\n{FR_GREEN}---------- That's all for today 👌 ----------{NO_COLOR}\n")
 
