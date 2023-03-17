cb072d3ca382fab2fa4a68beec9f6aa63364c5a7
InakiGainza <igg.git.h@gmail.com>
2023-03-17 12:07:30 +0100

    update openCmdLine

--- pythonConsoleTests/openCmdLine.py
+++ pythonConsoleTests/openCmdLine.py
@@ -1,5 +1,5 @@
 import os
 os.system('cls')
-os.system('cmd /c "py ./20230227-OS-Dir-Files-Example_For_Teacher.py"')
-#os.system('cmd /c "py ./pythonConsoleTests/20230227-OS-Dir-Files-Example_For_Teacher.py"')
 #os.system('cmd /k "color a & date"')
+os.system('cmd /c "20230227-OS-Dir-Files-Example_For_Teacher.py"')
+#os.system('cmd /c "./pythonConsoleTests/20230227-OS-Dir-Files-Example_For_Teacher.py"')
