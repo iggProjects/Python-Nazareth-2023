224b4fa88530ec8d55a12f05c2708a3aeb0f69ae
InakiGainza <igg.git.h@gmail.com>
2023-03-28 09:19:17 +0200

    update foods app

--- app_Foods/app_Foods_DB.py
+++ app_Foods/app_Foods_DB.py
@@ -90,5 +90,19 @@ def index():
 
     return render_template('index.html', entrada=entrada, ppal=ppal, postre=postre)
 
+@app.route('/login',methods=['GET', 'POST'])
+def login():      
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            #return redirect(url_for('home'))
+            return render_template('home_admin')
+        
+    return render_template('login.html', error=error)
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
