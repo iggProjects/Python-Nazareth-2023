f638bc79b7d74bd499dc1526b0d680cece6b4537
IñakiGainza <igg.git.h@gmail.com>
2023-04-17 17:29:43 +0200

    upd STRINGS-2

--- exam_practice/20230417-strings-2.py
+++ exam_practice/20230417-strings-2.py
@@ -1,10 +1,23 @@
 # page 9
-
 texto = "This is a Long text and Only the letters with Capital letters will be Included"
+texto2 = "This is a    Long     text    and Only the letters with Capital letters will be Included"
+print(f"first text: {texto}")
+print(f"second text: {texto2}")
+
+
 a = texto.split(" ")
 print(a)
+print()
+
+upperLettersInText = []
 for i in a:
   print(i[0])
+  if i[0].isupper():
+     upperLettersInText.append(i[0])   
+print(f"list of Upper Letters\n\t{upperLettersInText}\n")
+print()
 
 a_lower = [i.lower() for i in a]
-print(sorted(a_lower))
+print(f"list of Upper Letters\n\t{sorted(a_lower)}")
+#print(sorted(a_lower))
+
